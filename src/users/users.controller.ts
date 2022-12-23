46f37bc72f62a4c0bab89ce3918690f1525fc6d7
Optimisuke <naosuke0219@gmail.com>
2022-12-23 09:11:10 +0900

    second commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './interfaces/user.interface';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    await this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.find();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<User> {
+    return this.usersService.findOne(id);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.usersService.delete(id);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,59 @@
+import { Injectable, Inject, NotFoundException } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { Db } from 'mongodb';
+
+@Injectable()
+export class UsersService {
+  // findAll(): User[] | PromiseLike<User[]> {
+  //   throw new Error('Method not implemented.');
+  // }
+  constructor(
+    @Inject('DATABASE_CONNECTION')
+    private db: Db,
+  ) {}
+
+  async find(): Promise<User[]> {
+    return await this.db.collection<User>('users').find().toArray();
+  }
+
+  async findOne(id: string): Promise<User> {
+    const response = await this.db
+      .collection<User>('users')
+      .findOne({ _id: id });
+
+    if (!response) {
+      throw new NotFoundException();
+    }
+
+    return response;
+  }
+
+  async create(body: CreateUserDto): Promise<void> {
+    await this.db.collection('users').insertOne(body);
+  }
+
+  async update(id: string, body: UpdateUserDto): Promise<void> {
+    await this.db.collection('users').updateOne(
+      {
+        _id: id,
+      },
+      {
+        $set: {
+          ...body,
+        },
+      },
+    );
+  }
+
+  async delete(id: string): Promise<void> {
+    const response = await this.db.collection('users').deleteOne({
+      _id: id,
+    });
+
+    if (response.deletedCount === 0) {
+      throw new NotFoundException();
+    }
+  }
+}
